7ae617712652744b004808e3a86341dcaac36ab8
lopes-vincent <vlopes@openstudio.fr>
2015-05-26 11:16:08 +0200

    Initial commit

---
+++ GoogleShopping.php
@@ -0,0 +1,30 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace GoogleShopping;
+
+use Thelia\Module\BaseModule;
+
+set_include_path(get_include_path() . PATH_SEPARATOR . '/Google/src');
+
+class GoogleShopping extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'googleshopping';
+
+    /*
+     * You may now override BaseModuleInterface methods, such as:
+     * install, destroy, preActivation, postActivation, preDeactivation, postDeactivation
+     *
+     * Have fun !
+     */
+}
