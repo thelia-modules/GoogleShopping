aff36db0b9b3d1fd35a487541a986e79006dfe22
lopes-vincent <vlopes@openstudio.fr>
2015-05-27 10:13:32 +0200

    Update product insertion

--- Controller/Admin/ProductController.php
+++ Controller/Admin/ProductController.php
@@ -18,39 +18,33 @@ class ProductController extends BaseGoogleShoppingController
         $this->authorization();
 
         $product = new \Google_Service_ShoppingContent_Product();
-        $product->setOfferId("123");
-        $product->setTitle("Testing product");
-        $product->setDescription('A classic novel about the French Revolution');
-        $product->setLink('http://my-book-shop.com/tale-of-two-cities.html');
-        $product->setImageLink('http://my-book-shop.com/tale-of-two-cities.jpg');
-        $product->setContentLanguage('en');
-        $product->setTargetCountry('GB');
-        $product->setChannel('online');
-        $product->setAvailability('in stock');
+        $product->setChannel('local');
+        $product->setContentLanguage('fr');
+        $product->setOfferId('123');
+        $product->setTargetCountry('FR');
+        $product->setGtin('3001234567892');
+        $product->setBrand('test');
+        $product->setGoogleProductCategory('Animals & Pet Supplies > Live Animals');
         $product->setCondition('new');
-        $product->setGoogleProductCategory('Media > Books');
-        $product->setGtin('9780007350896');
+        $product->setLink('http://gshopping.openstudio-lab.com/?view=product&locale=en_US&product_id=22');
+        $product->setTitle('test');
+        $product->setAvailability('in stock');
 
         $price = new \Google_Service_ShoppingContent_Price();
-        $price->setValue('2.50');
-        $price->setCurrency('GBP');
+        $price->setValue('3');
+        $price->setCurrency('EUR');
 
         $shipping_price = new \Google_Service_ShoppingContent_Price();
-        $shipping_price->setValue('0.99');
-        $shipping_price->setCurrency('GBP');
+        $shipping_price->setValue('1');
+        $shipping_price->setCurrency('EUR');
 
         $shipping = new \Google_Service_ShoppingContent_ProductShipping();
         $shipping->setPrice($shipping_price);
-        $shipping->setCountry('GB');
+        $shipping->setCountry('FR');
         $shipping->setService('Standard shipping');
-
-        $shipping_weight = new \Google_Service_ShoppingContent_ProductShippingWeight();
-        $shipping_weight->setValue(200);
-        $shipping_weight->setUnit('grams');
 
         $product->setPrice($price);
         $product->setShipping(array($shipping));
-        $product->setShippingWeight($shipping_weight);
 
         $result = $this->service->products->insert(GoogleShopping::getConfigValue('client_id'), $product);
     }
