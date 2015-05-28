e90c39533bad9ac882503f1ff1f6d731171a04ab
Vincent Lopes-Vicente <vlopesvicente@gmail.com>
2015-05-28 22:39:18 +0200

    Add gtin check

--- Controller/Admin/ProductController.php
+++ Controller/Admin/ProductController.php
@@ -174,6 +174,12 @@ class ProductController extends BaseGoogleShoppingController
 
         //TODO : Add better check to verify validity of GTIN
         if (null === $pse->getEanCode()) {
+            throw new \Exception("Empty GTIN (EAN) code");
+        }
+
+        $checkGtin = $this->checkGtin($pse->getEanCode());
+
+        if (false === $checkGtin) {
             throw new \Exception("Invalid GTIN (EAN) code");
         }
 
@@ -331,4 +337,65 @@ class ProductController extends BaseGoogleShoppingController
 
         return $imageEvent;
     }
+
+    protected function checkGtin($gtin)
+    {
+        $cleanedCode = $this->clean($gtin);
+
+        return $this->isValidGtin($cleanedCode);
+    }
+
+    protected function clean($gtin, $fill = 14){
+        if (is_numeric($gtin)) {
+            return $this->zfill($gtin,$fill);
+        } elseif (is_string($gtin)) {
+            return $this->zfill(trim(str_replace("-", "", $gtin)), $fill);
+        }
+        return false;
+    }
+
+    protected function isValidGtin($cleanedCode)
+    {
+        if (!is_numeric($cleanedCode)) {
+            return false;
+        } elseif (!in_array(strlen($cleanedCode), array(8, 12, 13, 14, 18))) {
+            return false;
+        }
+
+        return $this->isGtinChecksumValid($cleanedCode);
+    }
+
+    protected function isGtinChecksumValid($code)
+    {
+        $firstPart = substr($code, -1);
+        $checkSum = $this->gtinCheckSum(substr(0, strlen($code)-1));
+        return $firstPart == $checkSum;
+    }
+
+    protected function gtinCheckSum($code)
+    {
+        $total = 0;
+
+        $codeArray = str_split($code);
+        foreach ($codeArray as $i => $c) {
+            if ($i % 2 == 1) {
+                $total = $total + $c;
+            } else {
+                $total = $total + (3*$c);
+            }
+        }
+        $checkDigit = (10 - ($total % 10)) % 10;
+        return $checkDigit;
+    }
+
+    protected function zfill($int,$cnt) {
+        $int = intval($int);
+        $nulls = "";
+        for($i=0; $i<($cnt-strlen($int)); $i++) {
+            $nulls .= '0';
+        }
+        return $nulls.$int;
+    }
+
+
 }
